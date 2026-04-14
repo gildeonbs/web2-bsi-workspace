de93d02ea0f019ed5fb8e976e6e35849081c0440
gildeonbs <gildeon23@gmail.com>
2026-04-14 16:42:55 -0300

    Update the Create page

--- Razor_Pages_e_Entity_Framework_Core_no_ASP.NET_Core/ContosoUniversity/ContosoUniversity/ContosoUniversity/Pages/Students/Create.cshtml.cs
+++ Razor_Pages_e_Entity_Framework_Core_no_ASP.NET_Core/ContosoUniversity/ContosoUniversity/ContosoUniversity/Pages/Students/Create.cshtml.cs
@@ -25,10 +25,12 @@ namespace ContosoUniversity.Pages.Students
         }
 
         [BindProperty]
-        public Student Student { get; set; } = default!;
+        // public Student Student { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+
+        // Original scaffolded code vertion (overposting vulnerable)
+        /*public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
@@ -39,6 +41,42 @@ namespace ContosoUniversity.Pages.Students
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }*/
+
+        // This method version improve security using "TryUpdateModelAsync" to overposting prevent
+        /*public async Task<IActionResult> OnPostAsync()
+        {
+            var emptyStudent = new Student();
+
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                _context.Students.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }*/
+
+        // The following code uses the StudentVM view model to create a new student (View models provide an alternative way to prevent overposting)
+        public StudentVM StudentVM { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var entry = _context.Add(new Student());
+            entry.CurrentValues.SetValues(StudentVM);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
+
+
     }
 }
