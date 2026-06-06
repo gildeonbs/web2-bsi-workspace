5326e93c7a69a4ce23f34ade09b4c5157f03a1d2
gildeonbs <gildeon23@gmail.com>
2026-06-05 21:30:33 -0300

    feat(ui): add base styles and initial About & Privacy pages

--- EloDoacoes/EloDoacoes/EloDoacoes/Controllers/HomeController.cs
+++ EloDoacoes/EloDoacoes/EloDoacoes/Controllers/HomeController.cs
@@ -23,10 +23,17 @@ namespace EloDoacoes.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
